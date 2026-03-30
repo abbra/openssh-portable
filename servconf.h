c6838d4cbe1ffe70f4a8e350a94c9ef27999f089
Dmitry Belyavskiy <dbelyavs@redhat.com>
2026-03-30 09:33:41 +0300

    Apply patch 0054-gssapi-s4u.patch
    Patch-name: 0054-gssapi-s4u.patch
    Patch-id: 54
    From-dist-git-commit: 5faa755c8e53beca27579286e83ae3747ddf6a3e

--- servconf.h
+++ servconf.h
@@ -160,6 +160,9 @@ typedef struct {
 	int     gss_cleanup_creds;	/* If true, destroy cred cache on logout */
 	int     gss_deleg_creds;	/* If true, accept delegated GSS credentials */
 	int     gss_strict_acceptor;	/* If true, restrict the GSSAPI acceptor name */
+	int     gss_allow_s4u2self; /* 0=no, INT_MAX=yes (GSS_C_INDEFINITE), >0=ticket lifetime s */
+	char  **gss_proxy_services; /* S4U2Proxy target service principals */
+	u_int   num_gss_proxy_services;
 	int 	gss_store_rekey;
 	char   *gss_kex_algorithms;	/* GSSAPI kex methods to be offered by client. */
 	int     password_authentication;	/* If true, permit password
@@ -313,6 +316,7 @@ TAILQ_HEAD(include_list, include_item);
 		M_CP_STROPT(permit_user_env_allowlist); \
 		M_CP_STROPT(pam_service_name); \
 		M_CP_STROPT(gss_indicators); \
+		M_CP_STRARRAYOPT(gss_proxy_services, num_gss_proxy_services); \
 		M_CP_STRARRAYOPT(authorized_keys_files, num_authkeys_files); \
 		M_CP_STRARRAYOPT(allow_users, num_allow_users); \
 		M_CP_STRARRAYOPT(deny_users, num_deny_users); \
